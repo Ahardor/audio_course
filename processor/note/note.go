ff4c45732ca22c22ba6149dac9e5e7bedaed3237
Voronin Valentin Maksimovich <valvoronin@ozon.ru>
2024-03-30 13:08:32 +0300

    Get note

--- processor/note/note.go
+++ processor/note/note.go
@@ -1,5 +1,7 @@
 package note
 
+import "fmt"
+
 type Note int
 
 const (
@@ -35,4 +37,22 @@ func (n Note) String() string {
 		"Ab", "A", "A#",
 		"Bb", "B",
 	}[n]
+}
+
+// GetNote возвращает ноту с учетом нот с одинаковым набором частот.
+func GetNote(n Note) string {
+	switch n {
+	case NoteCd, NoteDb:
+		return fmt.Sprintf("%s/%s", NoteCd.String(), NoteDb.String())
+	case NoteDd, NoteEb:
+		return fmt.Sprintf("%s/%s", NoteDd.String(), NoteEb.String())
+	case NoteFd, NoteGb:
+		return fmt.Sprintf("%s/%s", NoteFd.String(), NoteGb.String())
+	case NoteGd, NoteAb:
+		return fmt.Sprintf("%s/%s", NoteGd.String(), NoteAb.String())
+	case NoteAd, NoteBb:
+		return fmt.Sprintf("%s/%s", NoteAd.String(), NoteBb.String())
+	default:
+		return n.String()
+	}
 }
