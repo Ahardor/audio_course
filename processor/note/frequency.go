881c73d0ca690db262cbac2a6855b20aad39fdd9
Voronin Valentin Maksimovich <valvoronin@ozon.ru>
2024-03-30 12:45:15 +0300

    note table refactoring

--- processor/note/frequency.go
+++ processor/note/frequency.go
@@ -6,10 +6,12 @@ const (
 	// Для работы с частотами подразумевается округление частоты до 2 знаков после запятой.
 	// Data source: https://mixbutton.com/mixing-articles/music-note-to-frequency-chart/.
 	FrequencyBaseUnit = 100
+	// OctavesCount количество октав.
+	OctavesCount = 9
 )
 
 // Frequency - частота ноты для октавы в базовых единицах.
 type Frequency int
 
 // FrequencyList - набор частот, характерных для ноты в 0-8 октавах.
-type FrequencyList [9]Frequency
+type FrequencyList [OctavesCount]Frequency
--- processor/note/note.go
+++ processor/note/note.go
@@ -6,14 +6,33 @@ const (
 	NoteUnknown Note = iota
 	NoteC
 	NoteCd
+	NoteDb
 	NoteD
 	NoteDd
+	NoteEb
 	NoteE
 	NoteF
 	NoteFd
+	NoteGb
 	NoteG
 	NoteGd
+	NoteAb
 	NoteA
 	NoteAd
+	NoteBb
 	NoteB
 )
+
+// String возвращает строку с нотационной записью ноты.
+func (n Note) String() string {
+	return [...]string{
+		"Unknown",
+		"C", "C#",
+		"Db", "D", "D#",
+		"Eb", "E",
+		"F", "F#",
+		"Gb", "G", "G#",
+		"Ab", "A", "A#",
+		"Bb", "B",
+	}[n]
+}
